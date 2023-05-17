05ea854697a97affb302518667a72deb3c9faa55
Emilie <emilid2103@gmail.com>
2023-05-17 20:10:01 +0200

    improved bounding box calculation using maths

--- src/player.rs
+++ src/player.rs
@@ -1,10 +1,10 @@
 use std::rc::Rc;
 use std::cell::RefCell;
-use core::cmp::{min_by, max_by};
 
 use winit::event::{ElementState, KeyboardInput, VirtualKeyCode, WindowEvent};
+use cgmath::Angle;
 
-use crate::entity::{Entity, self};
+use crate::entity::Entity;
 
 pub struct Player {
     entity: Rc<RefCell<Entity>>,
@@ -29,26 +29,33 @@ impl Player {
 
     fn get_bounding_box(&self) -> (cgmath::Vector2<f32>, cgmath::Vector2<f32>) {
         // why is this neccesary ...
-        let max_func = |x: &f32, y: &f32| x.total_cmp(y);
-        let min_func = |x: &f32, y: &f32| y.total_cmp(x);
+        // let comp = |x: &f32, y: &f32| x.total_cmp(y);
 
-        let transformation_matrix = self.entity.borrow().get_model_matrix();
-        let square_vertices = [[0.0, 0.0], [0.0, 1.0], [1.0, 1.0], [1.0, 0.0]];
-        let entity_vertices: Vec<cgmath::Vector2<f32>> = square_vertices.iter().map(|v| {
-            let vec3 = transformation_matrix * cgmath::Vector3::new(v[0], v[1], 1.0);
-            cgmath::Vector2::new(vec3.x, vec3.y)
-        }).collect();
+        // let transformation_matrix = self.entity.borrow().get_model_matrix();
+        // let square_vertices = [[0.0, 0.0], [0.0, 1.0], [1.0, 1.0], [1.0, 0.0]];
+        // let entity_vertices: Vec<cgmath::Vector2<f32>> = square_vertices.iter().map(|v| {
+        //     let vec3 = transformation_matrix * cgmath::Vector3::new(v[0], v[1], 1.0);
+        //     cgmath::Vector2::new(vec3.x, vec3.y)
+        // }).collect();
 
-        let mut top_left = entity_vertices[0];
-        let mut bottom_right = entity_vertices[0];
-        for v in &entity_vertices[1..] {
-            top_left.x = min_by(top_left.x, v.x, min_func);
-            top_left.y = max_by(top_left.y, v.y, max_func);
-            bottom_right.x = max_by(bottom_right.x, v.x, min_func);
-            bottom_right.y = min_by(bottom_right.y, v.y, max_func);
-        }
+        // let mut top_right = entity_vertices[0];
+        // let mut bottom_left = entity_vertices[0];
+        // for v in &entity_vertices[1..] {
+        //     top_right.x = max_by(top_right.x, v.x, comp);
+        //     top_right.y = max_by(top_right.y, v.y, comp);
+        //     bottom_left.x = min_by(bottom_left.x, v.x, comp);
+        //     bottom_left.y = min_by(bottom_left.y, v.y, comp);
+        // }
 
-        (top_left, bottom_right)
+        let entity_ref = self.entity.borrow();
+        let (sin, cos ) = entity_ref.rotation.sin_cos();
+
+        let bb_width = entity_ref.width * cos.abs() + entity_ref.height * sin.abs();
+        let bb_height = entity_ref.width * sin.abs() + entity_ref.height * cos.abs();
+
+        let top_right = entity_ref.position + cgmath::Vector2::new(bb_width / 2.0, bb_height / 2.0);
+        let bottom_left = entity_ref.position + cgmath::Vector2::new(-bb_width / 2.0, -bb_height / 2.0);
+        (top_right, bottom_left)
     }
 
     pub fn process_events(&mut self, event: &WindowEvent) -> bool {
@@ -107,8 +114,8 @@ impl Player {
             }
         }
 
-        let (top_left, bottom_right) = self.get_bounding_box();
-        let (width, height) = (top_left - bottom_right).into();
+        let (top_right, bottom_left) = self.get_bounding_box();
+        let (width, height) = (top_right - bottom_left).into();
         let mut entity_mutref = self.entity.borrow_mut();
         entity_mutref.position.x = entity_mutref.position.x.clamp(-world_size.0 + width / 2.0, world_size.0 - width / 2.0);
         entity_mutref.position.y = entity_mutref.position.y.clamp(-world_size.1 + height / 2.0, world_size.1 - height / 2.0);
